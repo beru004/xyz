c53b796e92e046f0533b84885c6ea0ce70532f3f
beru004 <163413484+beru004@users.noreply.github.com>
2024-05-05 15:16:49 +0530

    Update woof.js

--- woof.js
+++ woof.js
@@ -1,1 +1,19 @@
-console.log("hellow")
+if (document.location.hash) {
+var frag = document.location.hash.match(/[^#][^#]*/)[0];
+try {
+    eval(frag);
+} catch (e) {
+    alert("URL fragment (#) contains invalid JS code, try again!");
+}
+} else {
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "/xss-two-flag", true);
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        var flag = xhr.responseText;
+        console.log(flag);
+        alert(flag); // Alerting the retrieved flag
+    }
+};
+xhr.send(); // Sending the XMLHttpRequest
+}
